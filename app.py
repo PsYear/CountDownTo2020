26d05880d62bb062a0ab5e8612ea3b4fcda181d7
孙泽闻 <sunzewen@wendellsun.local>
2019-12-31 20:09:39 +0800

    count down

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('count_down.html')
 
+@app.route('/2020')
+def happynewyear():
+    return render_template('newyear.html')
+
 
 if __name__ == '__main__':
     app.run()
